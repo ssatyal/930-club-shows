1caa63749f30bde1d5ef7c21765c3e7609a02121
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-04-14 11:12:17 -0400

    express yields layouts to index view, no data in browser YET

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require("express");
 var hbs = require("express-handlebars");
-// var mongoose = require("mongoose"); this might come from db/connection
+var mongoose = require("./db/connection"); //this might come from db/connection
+var parser = require("body-parser");
 var app = express();
+var Show = mongoose.model("Show");
 
-
+//sets port for local and heroku environments
 app.set("port", process.env.PORT || 3001);
-app.listen(app.get("port"), function(){
-  console.log("help, I'm alive");
-});
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:       ".hbs",
+  partialsDir:   "views/",
+  layoutsDir:    "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("WORKING");
+  res.render("shows-index");
+});
+
+app.listen(app.get("port"), function(){
+  console.log("help, I'm alive");
 });
