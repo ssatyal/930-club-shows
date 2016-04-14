55f3242734ae9a8bb9b0c1de304aaba1e97f0b67
Brendan Magee <bmagee@gmail.com>
2016-04-14 13:22:32 -0400

    added configuration for live push

--- db/connection.js
+++ db/connection.js
@@ -14,6 +14,12 @@ mongoose.model("Show", ShowSchema);
 
 // Need to add conditional when pushing live; db to be hosted at mLabs
 
-mongoose.connect("mongodb://localhost/930");
+// mongoose.connect("mongodb://localhost/930");
+
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGODB_URI);
+}else{
+  mongoose.connect("mongodb://localhost/930");
+}
 
 module.exports = mongoose;
